6fe5db012fb000a47c863aae5528b426a6ccbd87
aishwaryaprasher <ashwaryaprasher@gmail.com>
2018-06-07 00:05:02 +0530

    started assignment

---
+++ isha.py
@@ -0,0 +1,27 @@
+#a2_q1
+print("hello")
+
+#a2_q2
+print("acad"+"view")
+
+#a2_q3
+x="isha"
+y="jalandhar"
+z=21
+print("hello,i am %s, from %s,my age is %d"%(x,y,z))
+
+#a2_q4
+print('let\'s get started')
+
+#a2_q5
+s="acadview"
+course="python"
+fees=5000
+print("in %s,pursuing course %s,with fees %d"%(s,course,fees))
+
+#a2_q6
+name="tony stark"
+salary=1000000
+print("%s %d"%(name,salary))
+
+
