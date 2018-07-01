0e79a506da226f3dea8812fd70adbf800454282b
aishwaryaprasher <ashwaryaprasher@gmail.com>
2018-07-01 13:45:46 +0530

    assignment30june

--- lastdayoftraining.py
+++ lastdayoftraining.py
@@ -6,3 +6,33 @@ df=pd.DataFrame(data)
 print(df)
 df.loc[1]=['tom',22,'jerry@gmail.com',9876789898]
 print(df)
+
+#question2
+
+import numpy as np
+import pandas as pd
+data=pd.read_csv("weather.csv")
+df=pd.DataFrame(data)
+print("First five rows")
+print(df.head(5))
+print("First ten rows")
+print(df.head(10))
+print("Basic statistics")
+print("Axes-\n"+str(df.axes))
+print("Datatypes-\n"+str(df.dtypes))
+print("Dimensons-\n"+str(df.ndim))
+print("Shape-\n"+str(df.shape))
+print("Size\n"+str(df.size))
+print("\n")
+print("Last five rows")
+print(df.tail(5))
+
+
+x=df.loc[:,'Location']
+print(x)
+print("Basic statistics")
+print("Axes-\n"+str(x.axes))
+print("Datatypes-\n"+str(x.dtypes))
+print("Dimensons-\n"+str(x.ndim))
+print("Shape-\n"+str(x.shape))
+print("Size\n"+str(x.size))
